929a0dc2011d544f9854413b7fd403d86312e76c
Safouen Madiouni <safmad@amazon.fr>
2023-02-12 18:51:19 +0100

    new revision

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ import os
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
-    return "Flask inside Docker!!"
+    return "Flask inside Docker!! - new revision"
 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 80))
-    app.run(debug=True,host='0.0.0.0',port=port)
+    app.run(debug=True, host="0.0.0.0", port=port)
