984a6ebdf2906aa821df85b1d23fc58201bbffdb
Safouen Madiouni <safmad@amazon.fr>
2023-02-13 14:40:22 +0100

    add dummy unit tests

---
+++ tests/test_samples.py
@@ -0,0 +1,38 @@
+def test_case_1():
+    assert 1 == 1
+
+
+def test_case_2():
+    assert 1 == 1
+
+
+def test_case_3():
+    assert 1 == 1
+
+
+def test_case_4():
+    assert 1 == 1
+
+
+def test_case_5():
+    assert 1 == 1
+
+
+def test_case_6():
+    assert 1 == 1
+
+
+def test_case_7():
+    assert 1 == 1
+
+
+def test_case_8():
+    assert 1 == 1
+
+
+def test_case_9():
+    assert 1 == 1
+
+
+def test_case_10():
+    assert 1 == 1
